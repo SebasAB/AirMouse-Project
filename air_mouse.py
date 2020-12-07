9022c395e033f84f90aea60858e4174ace37f2fa
Sebastián Aliaga <70956062+SebasAB@users.noreply.github.com>
2020-12-07 16:12:56 -0500

    Add files via upload

--- air_mouse.py
+++ air_mouse.py
@@ -10,12 +10,16 @@ def air_mouse():
 		line = f.readlines() #line will be a list that each list item will be, either a 1 or 0, or a line with x, y and z axis data
 		line = line[-1] #line will take the value of the last item (the newest) of the list
 		if (len(line)<=1): #if the line lenght only has 1 item then it is either left or right click
-			if click_count > 0: #if there's already clicked it will be released
-				mouse.release(Button, left)
-				click_count-=1
-			else: 
-				mouse.press(Button, left) #if its not clicked it will be clicked
-				click_count+=1
+			if (int(line)==0): #if the number is equal to 0 it means that right click is being, clicked
+				mouse.press(Button, right) #this click right click 
+				mouse.release(Button, left) #this release right click
+			else: #if it's not 0 it means it's 1 so left click will be clicked
+				if click_count > 0: #if there's already clicked it will be released
+					mouse.release(Button, left)
+					click_count-=1
+				else: 
+					mouse.press(Button, left) #if its not clicked it will be clicked
+					click_count+=1
 
 		else: 
 			accec = line.split(";") #this will separate into a new array the items in the last line, separated by ";"
@@ -31,6 +35,10 @@ def air_mouse():
 		time.sleep(0.05) #the program will wait 0.05s until the next iteration 
 
 
-if __name__ == "__main__":
-	air_mouse() #this just runs the air_mouse function 
+def drawing(): 
+	pass
 
+
+if __name__ == "__main__":
+	drawing() #this runs the drawing function
+	air_mouse() #this just runs the air_mouse function 
